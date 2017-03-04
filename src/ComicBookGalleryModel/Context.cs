40b82a09f05d2e79d2b2cb6961766fc9d8c00b5a
John Amann <johnaman01@gmail.com>
2017-03-04 15:22:01 -0500

    Added Context class that will mediate between app and db

---
+++ src/ComicBookGalleryModel/Context.cs
@@ -0,0 +1,15 @@
+﻿using ComicBookGalleryModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel
+{
+    class Context : DbContext
+    {
+        public DbSet<ComicBook> ComicBooks { get; set; }
+    }
+}
